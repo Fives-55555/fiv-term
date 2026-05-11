f33eed00ac1069bba157af23b1527be1fe5f3b05
Fives-55555 <linus.pohlmann@icloud.com>
2026-05-11 11:36:32 +0200

    control.rs aktualisieren

--- src/virtseq/control.rs
+++ src/virtseq/control.rs
@@ -12,15 +12,28 @@ pub trait TermControl {
     type Result<T>;
     fn clear_screen(&self, buf: &mut VirtSeqBuf) -> Self::Result<()>;
     /// The Cursor Position will be returned through the StdIn. So you need to process the Input to get the Cursor Postion.
-    fn get_cursor_pos(&mut self, buf: &mut VirtSeqBuf, user_data: usize) -> Self::Result<()>;
+    fn get_cursor_pos(&mut self, buf: &mut VirtSeqBuf, user_data: u8) -> Self::Result<()>;
     fn set_cursor_pos(&self, buf: &mut VirtSeqBuf, x: i16, y: i16) -> Self::Result<()>;
 }
 
+struct TermResp {
+    // allows up to 32s timeout but 30s should be max
+    timestamp: u16,
+    resp: RespType,
+    id: u8,
+}
+
+#[repr(u8)]
+pub enum RespType {
+    ReqCurPos,
+    ReqScrSize,
+}
+
 pub struct TermInfoConfig<'me> {
-    resp: Vec<(&'me [u8], usize)>,
+    resp: Vec<TermResp>,
     buf: Box<[u8]>,
     clear: &'me [u8],
-    get_pos_res: &'me [u8],
+    get_pos_res: ParsedStringCap<2>,
     get_pos_req: &'me [u8],
     set_pos: ParsedStringCap<2>,
 }
@@ -62,13 +75,15 @@ macro_rules! get_strs {(
         };
     };
     (_ $entry:expr, $buffer:expr, $id:ident, $name:literal, in, $io:expr) => {
-        let str = $entry.get_str(idx_name!(STRING, $name));
-        match *str.unwrap() {
-            TermInfoString::There(slice) => {
-                ParsedStringCap::parse_in(&mut $buffer, slice, io);
+        let $id = {
+            let str = $entry.get_str(idx_name!(STRING, $name));
+            match *str.unwrap() {
+                TermInfoString::There(slice) => {
+                    ParsedStringCap::<{$io.len()}>::parse_in(&mut $buffer as &mut Vec<u8>, slice as &[u8], $io).unwrap()
+                }
+                _=>return Err(ParseError::EmptyStrIn)
             }
-            _=>return Err(ParseError::EmptyStrIn)
-        }
+        };
     };
 }
 
@@ -81,7 +96,7 @@ impl TermInfoConfig<'_> {
             entry,
             buf,
             clear, "clear_screen";
-            get_pos_res, "user6";
+            get_pos_res, "user6", in, [(ExprType::Int), (ExprType::Int|m)];
             get_pos_req, "user7";
             set_pos, "cursor_address", out, [(0u8, ExprType::Int),(1u8, ExprType::Int)];
         );
